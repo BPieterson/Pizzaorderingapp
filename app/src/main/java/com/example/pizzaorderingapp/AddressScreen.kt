d478e7f56b2049115414e65f04f52bcfb297eb99
BPieterson <ST10519794@myemeris.edu.za>
2026-06-10 10:00:10 +0200

    Update AddressScreen.kt

--- app/src/main/java/com/example/pizzaorderingapp/AddressScreen.kt
+++ app/src/main/java/com/example/pizzaorderingapp/AddressScreen.kt
@@ -7,8 +7,6 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class AddressScreen : AppCompatActivity() {
     lateinit var txtStAddress : EditText
